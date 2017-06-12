228ec2fb4bc8f54a6e5d4eca5919424bfebebb63
Aaron Freidus <afreidus@exploreplanet3.com>
2017-06-12 14:24:27 -0400

    setting up test files for module

---
+++ test/test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+describe('fec', () => {
+	describe('Constructor', () => {
+		it('Should take a settings object and set valid properties on the class', () => {
+			// assert.()
+		})
+	})
+
+	describe('getItem', () => {
+
+	})
+
+	describe('setItem', () => {
+		
+	})
+
+	describe('getStore', () => {
+		
+	})
+
+	describe('addStore', () => {
+		
+	})
+
+	describe('errorReporting', () => {
+		
+	})
+})
